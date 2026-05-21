f8911edea9f044a7ec1fee0ec341ef4b4760970e
S4DAX <davide312.ds@gmail.com>
2026-05-21 18:45:20 +0200

    first commit

---
+++ build/robot_arms/CMakeFiles/robot_arms_uninstall.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/robot_arms_uninstall"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/robot_arms_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
